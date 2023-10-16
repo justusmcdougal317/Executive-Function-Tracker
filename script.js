89c7c2faa82134018ffd38b17944655e334f6931
justusmcdougal317 <justusmcdougal317@gmail.com>
2023-10-16 05:23:45 -0700

    script.js changes

--- script.js
+++ script.js
@@ -1,59 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
-    function updateHourBlocks() {
-      const now = new Date();
-      const currentHour = now.getHours();
-  
-     
-      for (let hour = 9; hour <= 17; hour++) {
-        const timeBlock = document.getElementById(`hour-${hour}`);
-        const textArea = timeBlock.querySelector("textarea");
-  
-        if (hour < currentHour) {
-         
-          timeBlock.classList.add("past");
-          textArea.setAttribute("disabled", "disabled");
-        } else if (hour === currentHour) {
-          
-          timeBlock.classList.add("present");
-        } else {
-         
-          timeBlock.classList.remove("past");
-        }
+  function updateHourBlocks() {
+    const now = new Date();
+    const currentHour = now.getHours();
+
+    for (let hour = 9; hour <= 17; hour++) {
+      const timeBlock = document.getElementById(`hour-${hour}`);
+      const textArea = timeBlock.querySelector("textarea");
+
+      if (hour < currentHour) {
+        timeBlock.classList.add("past");
+      } else if (hour === currentHour) {
+        timeBlock.classList.add("present");
+      } else {
+        timeBlock.classList.remove("past");
+        timeBlock.classList.remove("present");
       }
     }
-  
-   
-    function saveTextToLocalStorage() {
-      const now = new Date();
-      const currentHour = now.getHours();
-  
-      for (let hour = 9; hour <= 17; hour++) {
-        const timeBlock = document.getElementById(`hour-${hour}`);
-        const textArea = timeBlock.querySelector("textarea");
-        localStorage.setItem(`hour-${hour}`, textArea.value);
-      }
+  }
+
+  function saveTextToLocalStorage() {
+    for (let hour = 9; hour <= 17; hour++) {
+      const timeBlock = document.getElementById(`hour-${hour}`);
+      const textArea = timeBlock.querySelector("textarea");
+      localStorage.setItem(`hour-${hour}`, textArea.value);
     }
-  
-    
-    function loadTextFromLocalStorage() {
-      for (let hour = 9; hour <= 17; hour++) {
-        const timeBlock = document.getElementById(`hour-${hour}`);
-        const textArea = timeBlock.querySelector("textarea");
-        textArea.value = localStorage.getItem(`hour-${hour}`);
-      }
+  }
+
+  function loadTextFromLocalStorage() {
+    for (let hour = 9; hour <= 17; hour++) {
+      const timeBlock = document.getElementById(`hour-${hour}`);
+      const textArea = timeBlock.querySelector("textarea");
+      textArea.value = localStorage.getItem(`hour-${hour}`);
     }
-  
-    
-    updateHourBlocks();
-    loadTextFromLocalStorage();
-  
-  
-    setInterval(updateHourBlocks, 60000);
-  
-    
-    const saveButtons = document.querySelectorAll(".saveBtn");
-    saveButtons.forEach((button) => {
-      button.addEventListener("click", saveTextToLocalStorage);
-    });
+  }
+
+  updateHourBlocks();
+  loadTextFromLocalStorage();
+
+  setInterval(updateHourBlocks, 60000);
+
+  const saveButtons = document.querySelectorAll(".saveBtn");
+  saveButtons.forEach((button) => {
+    button.addEventListener("click", saveTextToLocalStorage);
   });
